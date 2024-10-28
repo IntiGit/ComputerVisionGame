b8d17d0f73ee42ffc5de753c8892a861e89b8966
Oliver Adler <oliver_elias.adler@hsbi.de>
2024-10-28 18:27:09 +0100

    generating and drawing random fruits

--- Project/Game/main.py
+++ Project/Game/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 import pygame
+import random
+from Fruit import Fruit
 
 from Player import Player
 
@@ -13,6 +15,8 @@ SCREEN_WIDTH = background_image.get_width()
 SCREEN_HEIGHT = background_image.get_height()
 SCREEN = [SCREEN_WIDTH, SCREEN_HEIGHT]
 useCamera = False
+MAX_FRUITS = 3
+SPAWN_INTERVAL = 1000
 
 
 def initCamera(screen, useCam=False):
@@ -44,8 +48,12 @@ def main():
     playerSprite = pygame.image.load("Assets/playerSprite.png")
     player = Player(screen.get_width() / 2, screen.get_height() - playerSprite.get_height(), playerSprite)
 
+    fruits = []
+    last_spawn_time = pygame.time.get_ticks()
+
     running = True
     while running:
+        current_time = pygame.time.get_ticks()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -56,6 +64,22 @@ def main():
 
         screen.blit(background_image, (0, 0))
 
+        if len(fruits) < MAX_FRUITS and (current_time - last_spawn_time) > SPAWN_INTERVAL:
+            fruit_type = random.choice(['Assets/banana', 'Assets/apple'])
+            speed = random.randint(2, 5)
+            
+            new_fruit = Fruit(fruit_type, speed, screen)
+            fruits.append(new_fruit)
+        
+           # Update the last spawn  time
+            last_spawn_time = current_time
+
+        # Update and draw each fruit
+        for fruit in fruits:
+            fruit.update_pos_Y() 
+            fruit.draw(screen) 
+
+            
         if useCamera:
             screen.blit(getCameraFrame(cap), (0, 0))
 
